3bc3b2132ef363cca91fc20a8e0dfcdfabaf5d69
Willy Ricardo <willyric17@gmail.com>
2021-03-19 01:20:26 +0700

    refactor main menu using listview

--- lib/MainMenuPage.dart
+++ lib/MainMenuPage.dart
@@ -1,42 +1,48 @@
+import 'dart:ui';
+
+import 'package:analyzer_plugin/utilities/pair.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'dart:math';
 
 class MainMenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var localization = AppLocalizations.of(context);
+    final List<Pair<String, String>> items = <Pair<String, String>>[
+      Pair(localization.cherryPicker, "/cherrypick"),
+    ];
+
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context).mainMenu)),
       body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/howto');
-                      },
-                      child: Text(
-                        AppLocalizations.of(context).howTo,
-                        textScaleFactor: 1.5,
-                      ))),
-              SizedBox(
-                width: 0,
-                height: 10,
-              ),
-              SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/cherrypick');
-                      },
-                      child: Text(
-                        AppLocalizations.of(context).cherryPicker,
-                        textScaleFactor: 1.5,
-                      )))
-            ]),
+        child: Container(
+          width: 320,
+          padding: EdgeInsets.all(16),
+          child: ListView.separated(
+              shrinkWrap: true,
+              separatorBuilder: (BuildContext context, int index) {
+                return Divider();
+              },
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                var colorIndex = min(index + 1, 9) * 100;
+                var background = Colors.red[colorIndex];
+                var foreground = background.computeLuminance() > 0.4
+                    ? Colors.grey[800]
+                    : Colors.grey[50];
+                return ListTile(
+                    tileColor: background,
+                    onTap: () {
+                      Navigator.pushNamed(context, items[index].last);
+                    },
+                    title: Center(
+                        child: Text(
+                      "${items[index].first}",
+                      style: TextStyle(color: foreground),
+                    )));
+              }),
+        ),
       ),
     );
   }
